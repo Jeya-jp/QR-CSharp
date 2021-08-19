55d23aa0d210d138e7974ce724a1d73ef3c29d1a
sachin <sachinabs.js@gmail.com>
2021-08-19 16:24:23 +0530

    on load updating

--- QRCodeDemo-master/QRCodeDemo-master/QRCodeDemo/Form1.Designer.cs
+++ QRCodeDemo-master/QRCodeDemo-master/QRCodeDemo/Form1.Designer.cs
@@ -39,10 +39,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(487, 331);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(365, 269);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(147, 49);
+            this.button1.Size = new System.Drawing.Size(110, 40);
             this.button1.TabIndex = 0;
             this.button1.Text = "Generate";
             this.button1.UseVisualStyleBackColor = true;
@@ -51,18 +50,16 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(137, 288);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Location = new System.Drawing.Point(103, 234);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(327, 30);
+            this.textBox1.Size = new System.Drawing.Size(246, 26);
             this.textBox1.TabIndex = 1;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(80, 38);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(60, 31);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(404, 196);
+            this.pictureBox1.Size = new System.Drawing.Size(303, 159);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
@@ -70,44 +67,45 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(76, 355);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(57, 288);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 17);
+            this.label1.Size = new System.Drawing.Size(31, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Text:";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(137, 355);
+            this.textBox2.Location = new System.Drawing.Point(103, 288);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(327, 22);
+            this.textBox2.Size = new System.Drawing.Size(246, 20);
             this.textBox2.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(68, 288);
+            this.label2.Location = new System.Drawing.Point(51, 234);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(21, 17);
+            this.label2.Size = new System.Drawing.Size(18, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "ID";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(721, 428);
+            this.ClientSize = new System.Drawing.Size(541, 348);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- QRCodeDemo-master/QRCodeDemo-master/QRCodeDemo/Form1.cs
+++ QRCodeDemo-master/QRCodeDemo-master/QRCodeDemo/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.OleDb;
 using System.Windows.Forms;
 
 namespace QRCodeDemo
@@ -17,29 +11,158 @@ namespace QRCodeDemo
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        async private void button1_Click(object sender, EventArgs e)
         {
+            // https://qr-attadance.000webhostapp.com/index.html?rollno=19mca889&id=mwZOKJ2lmx
             QRCoder.QRCodeGenerator QG = new QRCoder.QRCodeGenerator();
-            var MyData = QG.CreateQrCode(textBox1.Text, QRCoder.QRCodeGenerator.ECCLevel.H);
-            var code = new QRCoder.QRCode(MyData);
-            pictureBox1.Image = code.GetGraphic(50);
-            string validChars = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+            string[] array1 = new string[]
+               {
+                    "JZ4jCj2u4K",
+                    "xDSPiEe1IA",
+                    "CD1aqhZPzn",
+                    "4b1xEjNJkz",
+                    "FmIl58LpJA",
+                    "5lqRDljdYa",
+                     "Mfe5uVZxXP",
+                     "bCvPdrQvTg",
+                     "SEjmxpVjvd",
+                      "CXjwVYx0GM",
+                      "DCd3LwQXrH",
+                      "LQ245altFR",
+                      "mZ0IFDXPsk",
+                      "VC3Gz9KxMK",
+                      "0DuoIpsLJ3",
+                      "CiQQP1dsbF",
+                      "UfU6i5ZGgM",
+                      "fF4RiQFtGM",
+                      "1WSNf2lyMd",
+                      "Iewl5b5joy",
+                      "L9IDDLH3ir",
+                      "AWSn8NjYUH",
+                      "SaRwKzLKFd",
+                      "NwE1CnUIDX",
+                      "HQGIrnl0EH",
+                      "Hua3vLFYgK",
+                      "NGXqSTKu4C",
+                      "liYQLygoXA",
+                      "THLdWRVY4g",
+                      "iHqYYoz3HH",
+                      "mIYN3I30Vs",
+                      "ELW0Lh9hpT",
+                      "R7o7edj4vB",
+                      "jSm2hB1Nea",
+                      "gi0j2cgsKg",
+                      "3qPGTuDioQ",
+                      "huHpx9nvSo",
+                      "5DQUCaCO8h",
+                      "iBZEhijD5q",
+                      "4ulkPIHOSD",
+                      "Xusm4xQHZ1",
+                      "Oe3DiWLa6w",
+                      "MepT2xref9",
+                      "NkROn8wy6d",
+                      "i23F75u6dM",
+                      "0Dyd6zZLKe",
+                      "m7EoUgKVw4",
+                      "v3zBBB3QGj",
+                      "kC0zRW28sG",
+                      "HQOiaTv4Ei",
+                      "9sLB09fdTU",
+                      "MBxf8msjeH",
+                      "yNcB3R3gru",
+                      "LlmDuYQxp1",
+                      "jlLrfXUPfK",
+                      "IkMosTvI8W",
+                      "OKfF3rQEOJ",
+                      "cIC3sZ3bNS",
+                      "AedU90jmiw",
+                      "xZDug6pcja",
+                      "1nD90Unpku",
+                      "KqCqE3FQyl",
+                      "dFQvmPrgI2",
+                      "ZWLJhTc8BN",
+                      "Y6Btg5qerC",
+                      "fxOwz5ZC3Q",
+                      "e0qT38sztt",
+                      "AOcaIqXyeN",
+                      "PFXeY73cZY",
+                       "LLvY8jqCr9",
+                       "2wehN6kmSM",
+                       "NwJ56kEGcS",
+                       "nNpmEA1wtl",
+                       "VDIRjbDo8U",
+                       "X0DjyIPe43",
+                       "0iLrTq8Y9L",
+                       "XLLjXpMIe8",
+                       "hqoF4l2FZu",
+                       "5Hz5BVSWye",
+                       "mwZOKJ2lm9",
+                       "U8YlmHsd0S",
+                       "rZ6dd6FSpn",
+                       "oTKXo1ZsJ9",
+                       "nUAnM6YnKp",
+                       "eVpzZQghEp",
+                       "VDylklrNLF",
+                       "QGAREgtzxr",
+                       "1yXDf9WmcM",
+                       "Judr3NaHOe",
+                       "iHavouRY0W",
+                       "dUrfn34jDa",
+                       "eR0mBYm5s3",
+                       "uaJjCSRz6J",
+                       "J4c4hIEPFs",
+                       "7iBLzNZXpN",
+                       "trEUK016zF",
+                       "KNdRB7oE6n"
+               };
+
             Random random = new Random();
-            int length, i;
+            int index = random.Next(array1.Length);
 
-            length = 5;
-            char[] password = new char[length];
-            for (i = 0; i < length; i++)
-                password[i] = validChars[random.Next(0, validChars.Length)];
-            String str = new string(password);
-            for (i = 0; i < length; i++)
-                textBox2.Text = str;
-          
+            //var id = array1[index];
+            var id = "Xusm4xQHZ1";
 
+
+            DateTime dateTime = DateTime.UtcNow.Date;
+            var date = dateTime.ToString("dd/MM/yyyy");
+            
+
+            System.Data.OleDb.OleDbConnection conn = new System.Data.OleDb.OleDbConnection();
+            conn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;" +
+              @"Data source= D:\git testing\get\c# app\QR-CSharp\QRCodeDemo-master\QRCodeDemo-master\onedata.accdb";
+
+            try
+            {
+                conn.Open();
+                String my_querry = "SELECT ID,today FROM details WHERE ID = '" + id + ", today = '" + date + "'";
+                OleDbCommand cmd = new OleDbCommand(my_querry, conn);
+                using (OleDbDataReader reader = cmd.ExecuteReader())
+                {                
+                    while (reader.Read())
+                    {
+                        MessageBox.Show("have");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //conn.Open();
+                String my_querry = "INSERT INTO details(ID,today)VALUES('" + id + "','" + date + "')";
+
+                OleDbCommand cmd = new OleDbCommand(my_querry, conn);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("created");
+            }
 
         }
 
         private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
